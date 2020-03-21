5a21e7e82c4425fb961ba4393f2543136f9edb41
Nakano Yusuke <nakasuke0509.info@gmail.com>
2020-03-21 12:22:34 +0900

    リストの削除機能を追加

--- kanban/views.py
+++ kanban/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, resolve_url
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, UpdateView, CreateView, ListView
+from django.views.generic import DetailView, UpdateView, CreateView, ListView, DeleteView
 
 from .forms import UserForm, ListForm
 from .mixins import OnlyYouMixin
@@ -70,3 +70,9 @@ class ListUpdateView(LoginRequiredMixin, UpdateView):
 
     def get_success_url(self):
         return resolve_url('kanban:lists_detail', pk=self.kwargs['pk'])
+
+class ListDeleteView(LoginRequiredMixin, DeleteView):
+    model = List
+    template_name = "kanban/lists/delete.html"
+    form_class = ListForm
+    success_url = reverse_lazy("kanban:lists_list")
