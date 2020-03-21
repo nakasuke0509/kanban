a0ff71b4161e33f7ab1c9bf40986151154dfb551
Nakano Yusuke <nakasuke0509.info@gmail.com>
2020-03-21 10:21:41 +0900

    ユーザー詳細画面の作成

--- kanban/views.py
+++ kanban/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.views.generic import DetailView
 
 def index(request):
     return render(request, "kanban/index.html") 
@@ -24,3 +26,7 @@ def signup(request):
         "form": form
     }
     return render(request, 'kanban/signup.html', context)
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = "kanban/users/detail.html"
