a8e60639818151d896b8b5a0740bb7a89bcbced4
Nakano Yusuke <nakasuke0509.info@gmail.com>
2020-03-21 10:50:23 +0900

    ユーザー編集画面を作成
    ・ユーザー詳細画面に編集リンクを設置
    ・自分以外編集できないように設定

--- kanban/views.py
+++ kanban/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, resolve_url
 from django.contrib.auth.decorators import login_required
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView
+
+from .forms import UserForm
+from .mixins import OnlyYouMixin
 
 def index(request):
     return render(request, "kanban/index.html") 
@@ -30,3 +33,11 @@ def signup(request):
 class UserDetailView(DetailView):
     model = User
     template_name = "kanban/users/detail.html"
+
+class UserUpdateView(OnlyYouMixin, UpdateView):
+    model = User
+    template_name = "kanban/users/update.html"
+    form_class = UserForm
+
+    def get_success_url(self):
+        return resolve_url('kanban:users_detail', pk=self.kwargs['pk'])
